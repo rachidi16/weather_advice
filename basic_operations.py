cecd3d2bf0d40664491f0981d8c75831382958e5
Fadoua Rachidi <fadouarachidi54@gmail.com>
2025-07-29 14:54:05 +0100

    Initial Django project setup

---
+++ basic_operations.py
@@ -0,0 +1,14 @@
+# basic_operations.py
+
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+sum_result = number1 + number2
+diff_result = number1 - number2
+product_result = number1 * number2
+
+# Print results in the required format
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {diff_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
